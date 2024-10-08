299e4d50c8cd339f6be586724ca42f8773313aa9
ALEXIS URQUIDEZ OSUNA <l2125010019@guasave.tecnm.mx>
2024-10-08 07:33:06 -0700

    add DProveedores

---
+++ CapaDatos/DProveedores.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class DProveedores
+    {
+        public DProveedores()
+        {
+
+        }
+        public DProveedores(int idproveedor, string nombre, string telefono,string direccion)
+        {
+            IDProveedor = idproveedor;
+            Nombre = nombre;
+            Telefono = telefono;
+            Direccion = direccion;
+        }
+
+        public static int IDProveedor {  get; set; }
+        public static string Nombre { get; set; }
+        public static string Telefono { get; set; }
+        public static string Direccion { get; set; }
+    }
+}
