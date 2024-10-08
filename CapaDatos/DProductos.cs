86e4f7670fc6f76129b7ef82211fbe2205c93f76
ALEXIS URQUIDEZ OSUNA <l2125010019@guasave.tecnm.mx>
2024-10-08 09:46:17 -0700

    add NProductos,DProductos Clases

---
+++ CapaDatos/DProductos.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class DProductos
+    {
+
+        public static Utilidades utilidades = new Utilidades(utilidades.SqlUserId, utilidades.SqlPassword);
+
+        public DProductos(int idProducto, string nombre, decimal precio, string descripcion, int idProveedor)
+        {
+            IdProducto = idProducto;
+            Nombre = nombre;
+            Precio = precio;
+            Descripcion = descripcion;
+            IdProveedor = idProveedor;
+        }
+
+        #region Propiedades
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Descripcion { get; set; }
+        public int IdProveedor { get; set; }
+        #endregion
+
+        #region MetodoInsertar
+        public string Insertar(DProductos producto)
+        {
+            string respuesta = "";
+            var conexionSql = utilidades.Conexion();
+
+            try
+            {
+                string consultaSql = "INSERT INTO Producto (Nombre, Precio, Descripcion, ID_Proveedor) VALUES (@nombre, @precio, @descripcion, @idProveedor)";
+                var comandoSql = new SqlCommand(consultaSql, conexionSql);
+
+                comandoSql.Parameters.AddWithValue("@nombre", producto.Nombre);
+                comandoSql.Parameters.AddWithValue("@precio", producto.Precio);
+                comandoSql.Parameters.AddWithValue("@descripcion", producto.Descripcion);
+                comandoSql.Parameters.AddWithValue("@idProveedor", producto.IdProveedor);
+
+                respuesta = comandoSql.ExecuteNonQuery() == 1 ? "Ok" : "No se pudo insertar el registro";
+            }
+            catch (Exception ex)
+            {
+                respuesta = ex.Message; // Consider logging the exception here.
+            }
+            finally
+            {
+                conexionSql.Close(); // Cerrar la conexión
+            }
+
+            return respuesta;
+        }
+        #endregion
+
+        #region MetodoEditar
+        public string Editar(DProductos producto)
+        {
+            string respuesta = "";
+            var conexionSql = utilidades.Conexion();
+
+            try
+            {
+                string consultaSql = "UPDATE Producto SET Nombre = @nombre, Precio = @precio, Descripcion = @descripcion, ID_Proveedor = @idProveedor WHERE ID_Producto = @idProducto";
+                var comandoSql = new SqlCommand(consultaSql, conexionSql);
+
+                comandoSql.Parameters.AddWithValue("@idProducto", producto.IdProducto);
+                comandoSql.Parameters.AddWithValue("@nombre", producto.Nombre);
+                comandoSql.Parameters.AddWithValue("@precio", producto.Precio);
+                comandoSql.Parameters.AddWithValue("@descripcion", producto.Descripcion);
+                comandoSql.Parameters.AddWithValue("@idProveedor", producto.IdProveedor);
+
+                respuesta = comandoSql.ExecuteNonQuery() == 1 ? "Ok" : "No se pudo editar el registro";
+            }
+            catch (Exception ex)
+            {
+                respuesta = ex.Message; // Consider logging the exception here.
+            }
+            finally
+            {
+                conexionSql.Close(); // Cerrar la conexión
+            }
+
+            return respuesta;
+        }
+        #endregion
+
+        #region MetodoEliminar
+        public string Eliminar(DProductos producto)
+        {
+            string respuesta = "";
+            var conexionSql = utilidades.Conexion();
+
+            try
+            {
+                string consultaSql = "DELETE FROM Producto WHERE ID_Producto = @idProducto";
+                var comandoSql = new SqlCommand(consultaSql, conexionSql);
+
+                comandoSql.Parameters.AddWithValue("@idProducto", producto.IdProducto);
+
+                respuesta = comandoSql.ExecuteNonQuery() == 1 ? "Ok" : "No se pudo eliminar el registro";
+            }
+            catch (Exception ex)
+            {
+                respuesta = ex.Message; // Consider logging the exception here.
+            }
+            finally
+            {
+                conexionSql.Close(); // Cerrar la conexión
+            }
+
+            return respuesta;
+        }
+        #endregion
+
+        #region MetodoMostrar
+        public DataTable Mostrar()
+        {
+            var resultadoTabla = new DataTable("producto");
+            var conexionSql = utilidades.Conexion();
+
+            try
+            {
+                string consultaSql = "SELECT TOP (1000) [ID_Producto], [Nombre], [Precio], [Descripcion], [ID_Proveedor] FROM [AbarroteDB].[dbo].[Producto]";
+                var comandoSql = new SqlCommand(consultaSql, conexionSql);
+
+                SqlDataAdapter sqlDat = new SqlDataAdapter(comandoSql);
+                sqlDat.Fill(resultadoTabla);
+            }
+            catch (Exception)
+            {
+                resultadoTabla = null;
+            }
+            finally
+            {
+                conexionSql.Close(); // Cerrar la conexión
+            }
+
+            return resultadoTabla;
+        }
+        #endregion
+
+        #region MetodoBuscarNombre
+        public DataTable BuscarNombre(string nombre)
+        {
+            var resultadoTabla = new DataTable("producto");
+            var conexionSql = utilidades.Conexion();
+
+            try
+            {
+                string consultaSql = "SELECT TOP (1000) [ID_Producto], [Nombre], [Precio], [Descripcion], [ID_Proveedor] FROM [AbarroteDB].[dbo].[Producto] WHERE Nombre LIKE @nombre + '%'";
+                var comandoSql = new SqlCommand(consultaSql, conexionSql);
+
+                comandoSql.Parameters.AddWithValue("@nombre", nombre);
+                SqlDataAdapter sqlDat = new SqlDataAdapter(comandoSql);
+                sqlDat.Fill(resultadoTabla);
+            }
+            catch (Exception)
+            {
+                resultadoTabla = null;
+            }
+            finally
+            {
+                conexionSql.Close(); // Cerrar la conexión
+            }
+
+            return resultadoTabla;
+        }
+        #endregion
+    }
+}
---
+++ CapaNegocio/NProductos.cs
@@ -0,0 +1,54 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class NProductos
+    {
+
+        #region InsertarProducto
+        public static string InsertarProducto(string nombre, decimal precio, string descripcion, int idProveedor)
+        {
+            DProductos producto = new DProductos(0, nombre, precio, descripcion, idProveedor);
+            return producto.Insertar(producto);
+        }
+        #endregion
+
+        #region EditarProducto
+        public static string EditarProducto(int idProducto, string nombre, decimal precio, string descripcion, int idProveedor)
+        {
+            DProductos producto = new DProductos(idProducto, nombre, precio, descripcion, idProveedor);
+            return producto.Editar(producto);
+        }
+        #endregion
+
+        #region EliminarProducto
+        public static string EliminarProducto(int idProducto)
+        {
+            DProductos producto = new DProductos(idProducto, string.Empty, 0, string.Empty, 0);
+            return producto.Eliminar(producto);
+        }
+        #endregion
+
+        #region MostrarProductos
+        public static DataTable MostrarProductos()
+        {
+            DProductos repo = new DProductos(0, string.Empty, 0, string.Empty, 0);
+            return repo.Mostrar();
+        }
+        #endregion
+
+        #region BuscarNombreProducto
+        public static DataTable BuscarNombreProducto(string textoBuscar)
+        {
+            DProductos repo = new DProductos(0, textoBuscar, 0, string.Empty, 0);
+            return repo.BuscarNombre(textoBuscar);
+        }
+        #endregion
+    }
+}
